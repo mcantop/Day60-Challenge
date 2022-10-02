2f342e432ad3a839572bd3d4979e7bf5c3159514
mcantop <maciejcantop@icloud.com>
2022-10-02 18:18:01 +0200

    Initial Commit

---
+++ Day60Challenge/Day60ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day60ChallengeApp.swift
+//  Day60Challenge
+//
+//  Created by Maciej on 02/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Day60ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
